f8de4c334a3439a35ea6df157ae7e04cd84c0422
ARVIND-36 <pnarvind06@gmail.com>
2026-05-24 14:35:01 +0530

    Add AKS deployment workflow

---
+++ terraform/aks.tf
@@ -0,0 +1,16 @@
+resource "azurerm_kubernetes_cluster" "cloudlab" {
+  name                = "cloudlab-aks"
+  location            = azurerm_resource_group.cloudlab.location
+  resource_group_name = azurerm_resource_group.cloudlab.name
+  dns_prefix          = "cloudlab"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size = "Standard_D2s_v3"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
